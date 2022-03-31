1ef1fae4a14bc5522c32383f822358ee6ac0702f
maxwellewxam <maxwellipe43662@gmail.com>
2022-03-30 21:16:41 -0500

    push this

--- AuthBakend.py
+++ AuthBakend.py
@@ -1,8 +1,7 @@
-from logging import debug
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
 	return 'Hello World!'
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, host='0.0.0.0', port=5678)
