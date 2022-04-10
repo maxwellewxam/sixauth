e99842ee297176876a927a19d0453950a4ee3310
maxwellewxam <maxwellipe43662@gmail.com>
2022-04-10 01:41:33 -0500

    i have alot to do to this but i am really loving VS code atm

--- Auth.py
+++ Auth.py
@@ -1,13 +1,16 @@
 '''An all-in-one user authenticator and data manager.'''
+
 import requests
 import subprocess
 import sys
 import os
+
 class LocationError(Exception): ...
 class AuthenticationError(Exception): ...
 class UsernameError(AuthenticationError): ...
 class PasswordError(AuthenticationError): ...
 class CryptError(Exception): ...
+
 class Auth:
     '''
     Main class of the Auth module. Set Path to desired data.json location.  Set Name and Pass to desired values.
@@ -18,6 +21,7 @@ class Auth:
 
     call repr(Auth) to retrive the current user.
     '''
+    
     def __init__(self, Path = None, Name = None, Pass = None):
         self.Name = Name
         self.Pass = Pass
@@ -28,8 +32,10 @@ class Auth:
         self.sesh = requests.Session()
         self.sesh.cert = ('ca-public-key.pem', 'ca-private-key.pem')
         rep = self.sesh.post(self.Path + 'Shake').json()
+        
     def __repr__(self):
         return self.Name
+    
     def __del__(self):
         try:
             self.sesh.put(self.Path+'Shake').json()
@@ -39,6 +45,7 @@ class Auth:
             self.server.terminate()
         except:
             print('couldnt close this')
+            
     def Save(self, Location, Data) -> bool:
         '''
         Saves specified data to specified location. Creates location if it doesn't exist.
@@ -46,6 +53,7 @@ class Auth:
         Auth.Save(['Loc1', 'Loc2',...], Data1) Saves Data1 to /Loc1/Loc2/
         '''
         return self.requestHandle(self.sesh.post(self.Path+'Data', {'Username':self.Name, 'Password':self.Pass, 'Location':Location, 'Data':Data}).json())
+    
     def Load(self, Location) -> str:
         '''
         Loads data at specified location. Raises an exception if location doesn't exist.
@@ -53,14 +61,13 @@ class Auth:
         Auth.Load(['Loc1', 'Loc2',...]) Returns data in /Loc1/Loc2/
         '''
         return self.requestHandle(self.sesh.put(self.Path+'Data', {'Username':self.Name, 'Password':self.Pass, 'Location':Location}).json())
+    
     def Login(self) -> bool:
         '''
         Attempts to login with previously specified Auth.Name and Auth.Pass values.
         
         Raises an exception if it fails.
         '''
-        #Pass = self.__Encrypt(self.Pass)
-        #name = self.__Encrypt(self.Name)
         return self.requestHandle(self.sesh.put(self.Path+'Auth', {'Username':self.Name, 'Password':self.Pass}).json())
         
     def Signup(self) -> bool:
@@ -70,6 +77,7 @@ class Auth:
         Raises an exception if it fails.
         '''
         return self.requestHandle(self.sesh.post(self.Path+'Auth', {'Username':self.Name, 'Password':self.Pass}).json())
+    
     def requestHandle(self, request):
         if request['Code'] == 200:
             return True
--- test.py
+++ test.py
@@ -1,23 +1,20 @@
 import Auth
-log = Auth.Auth()#'https://localhost:5678/')
-#log.Name = str(input('username: '))
-#log.Pass = str(input('password: '))
-log.Name = str('max')
-log.Pass = str('3008362')
-yeah = str(input('l or s: '))
-if yeah == 'l':
-    print(log.Login())
-elif yeah == 's':
-    print(log.Signup())
-load = str(input('load or save: '))
-if load == 'l':
-    print(log.Load('Data/fuck me/please/ong/buh/sdg'))
-else:
-    print(log.Save('Data/fuck me/please/ong/bruh/sdg', '42'))
-#except Auth.UsernameError as err:
-   # if str(err) == 'Username already exists':
-        #log.Login()
-    #else: raise Auth.UsernameError(err)
-#log._Auth__User = 'MAX'
-#log.Save(['brug'], 'this data is encrypted')
-#print(log.Load(['brug']))
+class MainApp:
+    def __init__(self) -> None:
+        self.mainloop()
+    def mainloop(self) -> None:
+        print('Welcome')
+        username = str(input('Enter Username: '))
+        password = str(input('Enter Password: '))
+        log = Auth.Auth(Name=username, Pass=password)
+        try:
+            log.Login()
+        except Auth.AuthenticationError as err:
+            if str(err) == 'Username does not exist':
+                try:
+                    log.Signup()
+                except Auth.AuthenticationError as err:
+                    print(err)
+            else: print(err)
+        
+MainApp()
