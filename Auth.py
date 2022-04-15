a875075f00d8c910b0d2e068de8e5b79d57fcb58
maxwellewxam <maxwellipe43662@gmail.com>
2022-04-14 23:03:18 -0500

    made auth request WAY faster when not connecting to dedicated server

--- Auth.py
+++ Auth.py
@@ -4,9 +4,16 @@ import requests
 import subprocess
 import sys
 import os
+import hashlib
+import jsonpath_ng
+import base64
 
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_restful import fields, marshal
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
 
 class LocationError(Exception): ...
 class AuthenticationError(Exception): ...
@@ -35,6 +42,29 @@ class Auth:
             app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
             app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
             db = SQLAlchemy(app)
+            
+            def Encrypt(Data, pas, nam):
+                kdf = PBKDF2HMAC(
+                    algorithm=hashes.SHA256(),
+                    length=32,
+                    salt=bytes(nam.encode()),
+                    iterations=390000,
+                    )
+                key = base64.urlsafe_b64encode(kdf.derive(bytes(pas.encode())))
+                fernet = Fernet(key)
+                return fernet.encrypt(Data.encode()).decode()
+
+            def Decrypt(Data, pas, nam):
+                kdf = PBKDF2HMAC(
+                    algorithm=hashes.SHA256(),
+                    length=32,
+                    salt=bytes(nam.encode()),
+                    iterations=390000,
+                    )
+                key = base64.urlsafe_b64encode(kdf.derive(bytes(pas.encode())))
+                fernet = Fernet(key)
+                return fernet.decrypt(Data.encode()).decode()
+    
             class DataMod(db.Model):
                 Username = db.Column(db.String, nullable=False, primary_key = True)
                 Password = db.Column(db.String, nullable=False)
@@ -44,15 +74,107 @@ class Auth:
                     self.Username = Username
                     self.Password = Password
                     self.Data = Data
+            
+            datfields = {'Data': fields.Raw}
+            passfields = {'Password': fields.String}
+            class Handle:
+                def __init__(self, Code):
+                    self.Code = Code
+                    
+                def json(self):
+                    return self.Code
+            def warp(func):
+                def wraper(*args, **kwargs):
+                        return Handle(func(*args, **kwargs))
+                return wraper
             class seshHandle:
                 def __init__(self):
                     pass
+                @warp
                 def post(self, location, data):
-                    print(location)
+                    if location == 'Auth':
+                        fromuser = data
+                        if fromuser['Username'] == '':
+                            return {'Code':406}
+                        if fromuser['Username'].isalnum() == False:
+                            return {'Code':406}
+                        fromdat = DataMod.query.filter_by(Username=fromuser['Username']).first()
+                        if fromdat:
+                            return {'Code':409}
+                        else:
+                            inf = DataMod(Username=fromuser['Username'], Password=hashlib.sha512((fromuser['Password'] + fromuser['Username']).encode("UTF-8")).hexdigest(), Data={})
+                            db.session.add(inf)
+                            db.session.commit()
+                            return {'Code':200}
+                    elif location == 'Data':
+                        fromuser = data
+                        if fromuser['Username'] == '':
+                            return {'Code':423}
+                        if fromuser['Username'].isalnum() == False:
+                            return {'Code':423}
+                        fromdat = DataMod.query.filter_by(Username=fromuser['Username']).first()
+                        if not fromdat:
+                            return {'Code':423}
+                        datPass = marshal(fromdat, passfields)['Password']
+                        userPass = hashlib.sha512((fromuser['Password'] + fromuser['Username']).encode("UTF-8")).hexdigest()
+                        if userPass == datPass:
+                            new = dict(marshal(fromdat, datfields)['Data'])
+                            try:
+                                jsonpath_ng.parse(data['Location'].replace('/', '.').replace(' ', '-')).update_or_create(new, Encrypt(data['Data'], data['Username'], data['Password']))
+                            except TypeError as err:
+                                if err == '\'str\' object does not support item assignment':
+                                    return {'Code':422}
+                            db.session.delete(fromdat)
+                            db.session.add(DataMod(Username=fromuser['Username'], Password=hashlib.sha512((fromuser['Password'] + fromuser['Username']).encode("UTF-8")).hexdigest(), Data=new))
+                            db.session.commit()
+                            return {'Code':200}
+                        else:
+                            return {'Code':423} 
+                    elif location == 'Shake':
+                        return {'Code':200}
+                @warp
                 def put(self, location, data):
-                    print(location)
-                def json(self, yes):
-                    pass
+                    if location == 'Auth':
+                        fromuser = data
+                        if fromuser['Username'] == '':
+                            return {'Code':406}
+                        if fromuser['Username'].isalnum() == False:
+                            return {'Code':406}
+                        fromdat = DataMod.query.filter_by(Username=fromuser['Username']).first()
+                        if not fromdat:
+                            return {'Code':404}
+                        datPass = marshal(fromdat, passfields)['Password']
+                        userPass = hashlib.sha512((fromuser['Password'] + fromuser['Username']).encode("UTF-8")).hexdigest()
+                        if userPass == datPass:
+                            return {'Code':200}
+                        else:
+                            return {'Code':401}
+                    elif location == 'Data':
+                        fromuser = data
+                        if fromuser['Username'] == '':
+                            return {'Code':423}
+                        if fromuser['Username'].isalnum() == False:
+                            return {'Code':423}
+                        fromdat = DataMod.query.filter_by(Username=fromuser['Username']).first()
+                        if not fromdat:
+                            return {'Code':423}
+                        datPass = marshal(fromdat, passfields)['Password']
+                        userPass = hashlib.sha512((fromuser['Password'] + fromuser['Username']).encode("UTF-8")).hexdigest()
+                        if userPass == datPass:
+                            farter = dict(marshal(fromdat, datfields)['Data'])
+                            try:
+                                jsonpath_expr = Decrypt([match.value for match in jsonpath_ng.parse(data['Location'].replace('/', '.').replace(' ', '-')).find(farter)][0], data['Username'], data['Password'])
+                            except IndexError as err:
+                                if str(err) == 'list index out of range':
+                                    return {'Code':416}
+                                else: 
+                                    raise IndexError(err)
+                            return {'Data':jsonpath_expr, 'Code':202}
+                        else:
+                            return {'Code':423}
+                    elif location == 'Shake':
+                        return {'Code':200}
+                
             self.sesh = seshHandle()
         else:
             self.sesh = requests.Session()
@@ -61,12 +183,13 @@ class Auth:
                 rep = self.sesh.post(self.Path + 'Shake').json()
             except requests.ConnectionError as err:
                 raise LocationError('Couldn\'t connect to backend server\nMessage:\n' + str(err))
+            pass
 
     def __repr__(self):
         return self.Name
     
     def __del__(self):
-        self.sesh.put(self.Path+'Shake').json()
+        self.sesh.put(self.Path+'Shake', 'JOE MOMMA').json()
         try:
             self.server.terminate()
         except:
