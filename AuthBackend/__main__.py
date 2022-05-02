7db5bdce778d3da01f9c0d9823c43b02af9394d2
maxwellewxam <maxwellipe43662@gmail.com>
2022-05-02 07:18:53 -0500

    aint no way

--- AuthBackend/__main__.py
+++ AuthBackend/__main__.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, fields, marshal
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
 from cryptography.fernet import Fernet
-from datetime import datetime
 
 import os
 import jsonpath_ng
