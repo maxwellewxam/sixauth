b883cb347134b86fb8425abc3d32ab10ac2e5162
max <3008362@students.ankenyschools.org>
2022-05-03 14:48:29 -0500

    wow all the functions of  Matrix works lol

--- Matrix/__main__.py
+++ Matrix/__main__.py
@@ -1,12 +1,12 @@
+from copy import deepcopy
 class Matrix():
     def __init__(self, Array):
         self.Array = Array
-        self.Array2 = self.copy()
     def copy(self):
-        Array = []
-        for item in enumerate(self.Array):
-            Array.append(item)
-        return Array
+        Array2 = []
+        for _, item in enumerate(self.Array):
+            Array2.append(deepcopy(item))
+        return Array2
     def index(self, row = 1, column = 1, value = 0):
         try:
             self.Array[row]
@@ -19,6 +19,7 @@ class Matrix():
         except:
             pass
         temp.insert(column, value)
+        return self
     def print(self):
         for i in range(len(self.Array)):
             for x in range(len(self.Array[i])):
@@ -29,8 +30,8 @@ class Matrix():
         for i in range(len(self.Array)):
             for x in range(len(self.Array[i])):
                 if self.Array[i][x] % mod != 0:
-                    self.Array2[i][x] = 0
-        return self.Array2
+                    self.Array[i][x] = 0
+        return self
     def sum(self):
         val = 0
         for i in range(len(self.Array)):
@@ -39,47 +40,40 @@ class Matrix():
         return val
     def average(self):
         return self.sum() / (len(self.Array) * len(self.Array[1]))
-    def rotate(self, turn = 1, Return = True):
+    def rotate(self, turn = 1):
         for _ in range(turn):
-            self.Array2 = list(zip(*self.Array2))[::-1]
-        if Return == True:
-            return [list(elem) for elem in self.arry2]
-        else:
-            self.Array = self.copy([list(elem) for elem in self.Array2])
-    def flip(self, direction = 'Row', Return = True):
-        import copy
-        if direction == 'Row':
-            for i in range(len(self.arry)):
-                self.arry2[i] = self.arry[i][::-1]
-        if direction == 'Column':
-            self.arry2 = self.arry[::-1]
-        if Return == True:
-            return self.arry2
-            self.arry2 = copy.deepcopy(self.arry)
-        else:
-            self.arry = copy.deepcopy(self.arry2)
+            self.Array2 = list(zip(*self.Array))[::-1]
+        self.Array = [list(elem) for elem in self.Array2]
+        return self
+    def flip(self, direction = 1):
+        self.Array2 = self.copy()
+        if direction == 1:
+            for i in range(len(self.Array)):
+                self.Array2[i] = self.Array[i][::-1]
+        if direction == 2:
+            self.Array2 = self.Array[::-1]
+        self.Array = self.Array2
+        del(self.Array2)
+        return self
 class Multiply(Matrix):
     def __init__(self, mA, mB):
-        self.arry = []
-        super().__init__(array = self.arry)
+        self.Array = []
         rowtemp = 0
         columntemp = 0
         temp = 0
-        if len(mA.arry[1]) == len(mB.arry):
-                for a in range(len(mA.arry)):
-                    for b in range(len(mB.arry[1])):
-                        for c in range(len(mB.arry)):
-                            temp += mA.arry[a][c] * mB.arry[c][b]
-                        self.index(row = rowtemp, column = columntemp, value = temp)
-                        temp = 0
-                        columntemp += 1
-                    rowtemp += 1
+        if len(mA.Array[1]) == len(mB.Array):
+            for a in range(len(mA.Array)):
+                for b in range(len(mB.Array[1])):
+                    for c in range(len(mB.Array)):
+                        temp += mA.Array[a][c] * mB.Array[c][b]
+                    self.index(row = rowtemp, column = columntemp, value = temp)
+                    temp = 0
+                    columntemp += 1
+                rowtemp += 1
 class Add(Matrix):
     def __init__(self, mA, mB):
-        import copy
-        self.arry = []
-        for i in range(len(mA.arry)):
-            for x in range(len(mB.arry[i])):
-                temp = mA.arry[i][x] + mB.arry[i][x]
+        self.Array = []
+        for i in range(len(mA.Array)):
+            for x in range(len(mB.Array[i])):
+                temp = mA.Array[i][x] + mB.Array[i][x]
                 self.index(row = i, column = x, value = temp)
-        super().__init__(array = self.arry)
