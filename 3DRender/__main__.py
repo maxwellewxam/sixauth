819c78297a9fcbf3ff4bc20e1cb7f10c3017d0b7
maxwellewxam <maxwellipe43662@gmail.com>
2022-05-07 02:39:00 -0500

    the damn projection matrix is not working

--- 3DRender/__main__.py
+++ 3DRender/__main__.py
@@ -1,9 +1,11 @@
+import pickle
 from MaxMods.Matrix import *
 from MaxMods.Canvas import *
 import keyboard as key
 import math
 import sys
-import numpy as np
+import cupy as np
+import numpy as AHH
 import pygame
 import fpstimer
 class cube:
@@ -118,15 +120,16 @@ class Renderer:
         self.anglex = 0
         self.angley = 0
         self.anglez = 0
-        self.sca = 10
+        self.sca = 2
         self.transx = 0
         self.transy = 0
-        self.transz = 50
+        self.transz = 533
         self.fov = 90
         self.near = .01
         self.far = 1080000
         self.a = 500/500
-        self.camera = [0,0,0,1]
+        self.camera = [0,0,0]
+        self.lookdir = [0,0,1]
         pygame.init()
         screen = pygame.display.set_mode([500, 500])
         running = True
@@ -148,15 +151,33 @@ class Renderer:
                 self.anglez += .1
             if key.is_pressed('q') is True:
                 self.anglez -= .1
-            self.f = 1/(np.tan((self.fov*.5)/(180*math.pi)))
-            self.prerspective = np.array([[self.a*self.f,0,0,0],[0,self.f,0,0],[0,0,(self.far/(self.far-self.near)),1], [0,0,(self.far*self.near)/(self.far-self.near),0]])
-            self.scale = np.array([[self.sca,0,0,0], [0,self.sca,0,0], [0,0,self.sca,0], [0,0,0,1]])
-            self.trans = np.array([[1,0,0,self.transx], [0,1,0,self.transy], [0,0,1,self.transz], [0,0,0,1]])
-            self.rotationx = np.array([[1,0,0,0],[0,float(np.cos(self.anglex)),float(-np.sin(self.anglex)),0],[0,float(np.sin(self.anglex)),float(np.cos(self.anglex)),0],[0,0,0,1]])
-            self.rotationy = np.array([[float(np.cos(self.angley)),0,float(np.sin(self.angley)),0],[0,1,0,0],[float(-np.sin(self.angley)),0,float(np.cos(self.angley)),0],[0,0,0,1]])
-            self.rotationz = np.array([[float(np.cos(self.anglez)),float(-np.sin(self.anglez)),0,0],[float(np.sin(self.anglez)),float(np.cos(self.anglez)),0,0],[0,0,1,0],[0,0,0,1]])
+            if key.is_pressed('up') is True:
+                self.camera[1] += 1
+            if key.is_pressed('down') is True:
+                self.camera[1] -= 1
+            if key.is_pressed('right') is True:
+                self.camera[0] -= 1
+            if key.is_pressed('left') is True:
+                self.camera[0] += 1
+            self.f = AHH.divide(1,(AHH.tan(AHH.divide(AHH.multiply(self.fov,.5),AHH.multiply(180,AHH.pi)))))
+            prerspective1 = AHH.array([[AHH.multiply(self.a,self.f),0,0,0],[0,float(self.f),0,0],[0,0,AHH.divide(self.far,AHH.subtract(self.far,self.near)),1], [0,0,AHH.divide(AHH.multiply(self.far,self.near),AHH.subtract(self.far,self.near)),0]])
+            prerspective = np._core.core.array(prerspective1)
+            scale = np.array([[self.sca,0,0,0], [0,self.sca,0,0], [0,0,self.sca,0], [0,0,0,1]])
+            trans = np.array([[1,0,0,self.transx], [0,1,0,self.transy], [0,0,1,self.transz], [0,0,0,1]])
+            rotationx = np.array([[1,0,0,0],[0,float(np.cos(self.anglex)),float(-np.sin(self.anglex)),0],[0,float(np.sin(self.anglex)),float(np.cos(self.anglex)),0],[0,0,0,1]])
+            rotationy = np.array([[float(np.cos(self.angley)),0,float(np.sin(self.angley)),0],[0,1,0,0],[float(-np.sin(self.angley)),0,float(np.cos(self.angley)),0],[0,0,0,1]])
+            rotationz = np.array([[float(np.cos(self.anglez)),float(-np.sin(self.anglez)),0,0],[float(np.sin(self.anglez)),float(np.cos(self.anglez)),0,0],[0,0,1,0],[0,0,0,1]])
+            self.up = np.array([0,-1,0])
+            target = np.add(np.array(self.camera), np.array(self.lookdir))
+            forward = np.subtract(np.array(target), np.array(self.camera))
+            nforward = forward/np.linalg.norm(forward)
+            up = np.subtract(self.up, np.multiply(nforward, np.dot(self.up, nforward)))
+            nup = up/np.linalg.norm(up)
+            nright = np.cross(nup, nforward)
+            pointmat = np.array([np.append(nright, target[0]), np.append(nup, target[1]), np.append(nforward, target[2]), np.array([0,0,0,1])])
+            viewmat = np.linalg.inv(pointmat)
             screen.fill((0, 0, 0))
-            screen.lock()
+            faces = []
             for a,b,c in self.faces:
                 triangle = np.array([
                     self.cubm[a],
@@ -164,33 +185,40 @@ class Renderer:
                     self.cubm[c]
                 ])
                 transtri = np.array([
-                    self.trans@self.rotationz@self.rotationy@self.rotationx@self.scale@triangle[0],
-                    self.trans@self.rotationz@self.rotationy@self.rotationx@self.scale@triangle[1],
-                    self.trans@self.rotationz@self.rotationy@self.rotationx@self.scale@triangle[2]
+                    viewmat@trans@rotationz@rotationy@rotationx@scale@triangle[0],
+                    viewmat@trans@rotationz@rotationy@rotationx@scale@triangle[1],
+                    viewmat@trans@rotationz@rotationy@rotationx@scale@triangle[2]
                 ])
                 cross = np.cross(np.subtract(transtri[1], transtri[0])[:-1], np.subtract(transtri[2], transtri[0])[:-1])
                 normal = cross/np.linalg.norm(cross)
-                #https://stackoverflow.com/questions/11994819/how-can-i-move-the-camera-correctly-in-3d-space  #go here
                 if (normal[0] * (transtri[0][0] - self.camera[0]) +
                     normal[1] * (transtri[0][1] - self.camera[1]) +
                     normal[2] * (transtri[0][2] - self.camera[2]) <= 0):
-                    light = [0,0,-1]
-                    nlight = light/np.linalg.norm(light)
+                    light = [0,0,1]
+                    nlight = np.array(light)/np.linalg.norm(np.array(light))
                     dp = np.dot(normal, nlight)
                     color = self.get_color(dp)
                     projected = np.array([
-                        self.prerspective@transtri[0],
-                        self.prerspective@transtri[1],
-                        self.prerspective@transtri[2]
+                        prerspective@transtri[0],
+                        prerspective@transtri[1],
+                        prerspective@transtri[2]
                     ])
-                    pygame.draw.polygon(screen, color, [i[:-2] for i in np.add(projected, 250)])
+                    faces.append(((projected,color), (transtri[0][2]+transtri[1][2]+transtri[2][2])/3))
+                    #pygame.draw.polygon(screen, color, [i[:-2] for i in np.add(projected, 250)])
+            screen.lock()
+            faces.sort(key=self.sorttttt)
+            faces.reverse()
+            for (item,color),_ in faces:
+                pygame.draw.polygon(screen, color, [i[:-2] for i in np.add(item, 250).tolist()])
             screen.unlock()
             pygame.display.update()
         pygame.quit()
     def get_color(self, colNum):
         rgbNum = abs(int(255 - ((1-colNum)*255.0)))
         return (rgbNum,rgbNum,rgbNum)
-
+    def sorttttt(self, n):
+        a,z = n
+        return z
 class ObjLoader(object):
     def __init__(self, fileName):
         self.vertices = []
