7d90ad95d2d61ebbc403fe897c1d59a230f9412f
max <3008362@students.ankenyschools.org>
2022-05-17 14:17:31 -0500

    fixed that pretty fast lol

--- Auth/__main__.py
+++ Auth/__main__.py
@@ -37,7 +37,7 @@ class AuthSesh:
             
             self.__Path = ''
             app = Flask(__name__)
-            app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+            app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.getcwd()}/database.db'
             app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
             db = SQLAlchemy(app)
 
