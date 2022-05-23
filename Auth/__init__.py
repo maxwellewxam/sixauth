db47d52f954273cad375c2b030c35a92284252ee
max <3008362@students.ankenyschools.org>
2022-05-23 14:47:06 -0500

    made this better i think

--- Auth/__init__.py
+++ Auth/__init__.py
@@ -1,3 +1,3 @@
 '''An all-in-one user authenticator and data manager'''
-from .__main__ import *
+from .Main.__main__ import *
 __all__ = ['LocationError', 'AuthenticationError', 'UsernameError', 'PasswordError', 'AuthSesh']
