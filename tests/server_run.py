74769a4d605a3545aece6201481e3bee3e42636b
maxwellewxam <maxwellipe43662@gmail.com>
2023-01-05 01:45:00 -0600

    silly mistake after silly misatake

--- tests/server_run.py
+++ tests/server_run.py
@@ -1,8 +1,8 @@
 
 import sys
 import os
-HERE = os.path.abspath('../')
-#HERE = os.path.abspath(os.getcwd())
+#HERE = os.path.abspath('../')
+HERE = os.path.abspath(os.getcwd())
 sys.path.append(HERE)
 from maxmods.auth.main import start_server
 start_server('127.0.0.1', 5678)
