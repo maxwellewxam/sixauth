957defebe0ee997fb1c313f7b40bd8543a933273
max <3008362@students.ankenyschools.org>
2023-02-13 13:22:43 -0600

    updated tests

--- tests/Auth_Test.py
+++ tests/Auth_Test.py
@@ -6,8 +6,8 @@ sys.path.append(HERE)
 from sixauth import AuthSesh as ash
 from sixauth.main import AuthenticationError, LocationError
 import unittest
-#setup_logger(server_logger_location=None, log_sensitive=True, log_more=True)
-with ash() as user1, ash('127.0.0.1:5678') as user2:
+# '127.0.0.1:5678'
+with ash('ldums.com:5678') as user1:
 
     class testAuth(unittest.TestCase):
         def test_111_login_client_side_wrong_username(self):
@@ -76,73 +76,6 @@ with ash() as user1, ash('127.0.0.1:5678') as user2:
         def test_199_remove_client_side_success(self):
             user1.set_vals('test', 'test')
             self.assertTrue(user1.remove())
-        
-        def test_211_login_server_side_wrong_username(self):
-            user2.set_vals('test', 'test')
-            with self.assertRaises(AuthenticationError) as cm:
-                user2.login()
-            the_exception = cm.exception
-            self.assertEqual(str(the_exception), 'Username does not exist')
-
-        def test_212_login_server_side_no_username(self):
-            user2.set_vals('', 'test')
-            with self.assertRaises(AuthenticationError) as cm:
-                user2.login()
-            the_exception = cm.exception
-            self.assertEqual(str(the_exception), 'Invalid username')
-        
-        def test_221_login_server_side_bad_pass(self):
-            user2.set_vals('test', 'max')
-            with self.assertRaises(AuthenticationError) as cm:
-                user2.login()
-            the_exception = cm.exception
-            self.assertEqual(str(the_exception), 'Incorrect password')
-        
-        def test_213_signup_server_side_success(self):
-            user2.set_vals('test', 'test')
-            self.assertTrue(user2.signup())
-        
-        def test_222_login_server_side_success(self):
-            user2.set_vals('test', 'test')
-            self.assertTrue(user2.login())
-        
-        def test_223_signup_server_side_bad_username(self):
-            user2.set_vals('test', 'test')
-            with self.assertRaises(AuthenticationError) as cm:
-                user2.signup()
-            the_exception = cm.exception
-            self.assertEqual(str(the_exception), 'Username already exists')
-        
-        def test_231_save_server_side_success(self):
-            user2.set_vals('test', 'test')
-            self.assertTrue(user2.save('Test/Test', 'UR MOM'))
-        
-        def test_232_load_server_side_success(self):
-            user2.set_vals('test', 'test')
-            self.assertEqual(user2.load('Test/Test'), 'UR MOM')
-            
-        def test_233_load_server_side_doesnt_exist(self):
-            user2.set_vals('test', 'test')
-            with self.assertRaises(LocationError) as cm:
-                user2.load('John/Green/Rubber/Co')
-            the_exception = cm.exception
-            self.assertEqual(str(the_exception), 'Loaction does not exist')
-            
-        def test_234_save_server_side_whole_dict(self):
-            user2.set_vals('test', 'test')
-            self.assertTrue(user2.save('', {'URMOM':'test'}))
-            
-        def test_235_load_server_side_all_data(self):
-            user2.set_vals('test', 'test')
-            self.assertEqual(user2.load(''), {'':{'URMOM':'test'}})
-        
-        def test_236_save_server_side_str(self):
-            user2.set_vals('test', 'test')
-            self.assertTrue(user2.save('', 'comma'))
-        
-        def test_299_remove_server_side_success(self):
-            user2.set_vals('test', 'test')
-            self.assertTrue(user2.remove())
             
     if __name__ == '__main__':
         unittest.main()
